ed2b87f291e81cd648c64a82df41120b54acd26d
Hossein Baloochian <59353973+hbaloochian@users.noreply.github.com>
2023-05-24 17:49:16 +0330

    Add files via upload

---
+++ 16/MyModule/MyModule.psm1
@@ -0,0 +1,2 @@
+﻿. $PSScriptRoot\Public\Get-Cname.ps1
+. $PSScriptRoot\Public\Get-Zone.ps1
---
+++ 16/MyModule/MyModule1.psm1
@@ -0,0 +1,2 @@
+﻿Import-Module $PSScriptRoot\Public\Get-Cname.ps1
+Import-Module $PSScriptRoot\Public\Get-Zone.ps1
